0fbb03e57b7b84d53d4d4996ffd91eafd1683e22
ummetarin <ummehomairatarin@gmail.com>
2024-04-21 23:40:33 +0600

    Complete my tik take toe game

--- Tik_take_game.c
+++ Tik_take_game.c
@@ -15,41 +15,38 @@ void main(){
      while (staus==-1)
      {
       player=(player%2==0)?2:1;
-   char mark=(player==1)?'X':'0';
-   printf("Please Enter the Number of Player  %d\n",player);
+      char mark=(player==1)?'X':'0';
+      printf("Please Enter the Number of Player  %d\n",player);
 
-  scanf("%d",&Giveplace);
+      scanf("%d",&Giveplace);
 
-  if(Giveplace<1 || Giveplace>9){
-    printf("Invalid input");
-  }
+      if(Giveplace<1 || Giveplace>9){
+        printf("Invalid input\n");
+        continue; // Restart the loop if input is invalid
+      }
 
-  board[Giveplace]=mark;
-    printBoard();
+      if(board[Giveplace] == 'X' || board[Giveplace] == 'O'){
+        printf("This place is already filled. Please choose another.\n");
+        continue; // Restart the loop if the place is already filled
+      }
 
-   int result=cheakWiner();
+      board[Giveplace]=mark;
+      printBoard();
 
-   if(result==1){
-    printf("Winner is =%d\n",player);
-   }
-   else if(result==0){
-    printf("Draw");
-    return;
-   }
+      int result=cheakWiner();
 
+      if(result==1){
+        printf("Winner is = %d\n",player);
+        staus = 1; // Set staus to stop the loop
+      }
+      else if(result==0){
+        printf("Draw\n");
+        return;
+      }
 
-
-    player++;
+      player++;
      }
-     
-
-
-
-
-  
-        
 }
-
 
 void printBoard(){
   system("cls");
@@ -69,46 +66,43 @@ void printBoard(){
 
 int cheakWiner(){
 
-if(board[1]==board[2]&&board[2]==board[3]){
-  return 1;
-}
-if(board[1]==board[4]&&board[4]==board[7]){
-  return 1;
-}
-if(board[7]==board[8]&&board[8]==board[9]){
-  return 1;
-}
-if(board[3]==board[6]&&board[6]==board[9]){
-  return 1;
-}
-if(board[1]==board[5]&&board[5]==board[9]){
-  return 1;
-}
-if(board[3]==board[5]&&board[5]==board[7]){
-  return 1;
-}
-if(board[2]==board[5]&&board[5]==board[8]){
-  return 1;
-}
-if(board[4]==board[5]&&board[5]==board[6]){
-  return 1;
-}
+  if(board[1]==board[2]&&board[2]==board[3]){
+    return 1;
+  }
+  if(board[1]==board[4]&&board[4]==board[7]){
+    return 1;
+  }
+  if(board[7]==board[8]&&board[8]==board[9]){
+    return 1;
+  }
+  if(board[3]==board[6]&&board[6]==board[9]){
+    return 1;
+  }
+  if(board[1]==board[5]&&board[5]==board[9]){
+    return 1;
+  }
+  if(board[3]==board[5]&&board[5]==board[7]){
+    return 1;
+  }
+  if(board[2]==board[5]&&board[5]==board[8]){
+    return 1;
+  }
+  if(board[4]==board[5]&&board[5]==board[6]){
+    return 1;
+  }
 
-int count=0;
+  int count=0;
 
-for ( int i = 1; i <= 9; i++)
-{
- if(board[i]=='X' || board[i]=='o'){
-  count++;
- }
-}
+  for ( int i = 1; i <= 9; i++)
+  {
+    if(board[i]=='X' || board[i]=='o'){
+      count++;
+    }
+  }
 
-if(count==9){
-  return 0;
-}
+  if(count==9){
+    return 0;
+  }
 
-
-return -1;
-
-
+  return -1;
 }
